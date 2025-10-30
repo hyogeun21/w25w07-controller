1fe9a533fe28faef5c87489d251610368bf24546
hyogeun21 <hgpark1515@naver.com>
2025-10-30 17:15:28 +0900

    👷Service

--- src/main/kotlin/kr/ac/kumoh/s20210524/w25w07_controller/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20210524/w25w07_controller/controller/SongController.kt
@@ -1,12 +1,11 @@
 package kr.ac.kumoh.s20210524.w25w07_controller.controller
 
+import kr.ac.kumoh.s20210524.w25w07_controller.service.SongService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SongController {
-    val title = listOf("Golder", "Blue Valentine", "타임캡슐")
-
+class SongController(private val service: SongService) {
     @GetMapping("/songs")
-    fun getSongList() = title
+    fun getSongList() = service.getAllSongs()
 }
---
+++ src/main/kotlin/kr/ac/kumoh/s20210524/w25w07_controller/model/Song.kt
@@ -0,0 +1,7 @@
+package kr.ac.kumoh.s20210524.w25w07_controller.model
+
+data class Song(
+    val id: Int,
+    val tilte: String,
+    val singer: String,
+)
